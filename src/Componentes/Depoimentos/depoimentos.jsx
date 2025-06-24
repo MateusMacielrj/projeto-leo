ed9fcbc194363da1adb5e0b1993bedfabf625200
Mateus Maciel <mateusmacielrj@gmail.com>
2025-06-24 15:20:19 -0300

    Adiciona modal no Depoimentos e ajustes nos estilos

--- src/Componentes/Depoimentos/depoimentos.jsx
+++ src/Componentes/Depoimentos/depoimentos.jsx
@@ -1,44 +1,51 @@
+import { useState } from "react";
 import ImagemAntesDepois from "../imagems/antesedepois.png";
 
 function Depoimentos() {
+  const [imagemModal, setImagemModal] = useState(null);
+
+  const abrirModal = (src) => {
+    setImagemModal(src);
+  };
+
+  const fecharModal = () => {
+    setImagemModal(null);
+  };
+
   return (
     <section id="depoimentos" className="depoimentos">
       <h3>Depoimentos</h3>
 
       <div className="alunos">
-        <div className="depoimento">
-          <img src={ImagemAntesDepois} alt="" />
-          <blockquote>
-            "Excelente profissional, sempre atento às minhas necessidades.
-            Recomendo demais!"
-          </blockquote>
-          <p>
-            <strong>- Carlos Souza</strong>
-          </p>
-        </div>
-
-        <div className="depoimento">
-          <img src={ImagemAntesDepois} alt="" />
-          <blockquote>
-            "Os treinos personalizados fizeram toda a diferença no meu
-            rendimento. Estou muito satisfeita!"
-          </blockquote>
-          <p>
-            <strong>- Maria Oliveira</strong>
-          </p>
-        </div>
-
-        <div className="depoimento">
-          <img src={ImagemAntesDepois} alt="" />
-          <blockquote>
-            "Excelente profissional, sempre atento às minhas necessidades.
-            Recomendo demais!"
-          </blockquote>
-          <p>
-            <strong>- Carlos Souza</strong>
-          </p>
-        </div>
+        {[1, 2, 3].map((_, i) => (
+          <div className="depoimento" key={i}>
+            <img
+              src={ImagemAntesDepois}
+              alt="Antes e depois"
+              onClick={() => abrirModal(ImagemAntesDepois)}
+              style={{ cursor: "pointer" }}
+            />
+            <blockquote>
+              "Excelente profissional, sempre atento às minhas necessidades.
+              Recomendo demais!"
+            </blockquote>
+            <p>
+              <strong>- Carlos Souza</strong>
+            </p>
+          </div>
+        ))}
       </div>
+
+      {imagemModal && (
+        <div className="modal" onClick={fecharModal}>
+          <div className="modal-conteudo" onClick={(e) => e.stopPropagation()}>
+            <img src={imagemModal} alt="Imagem ampliada" />
+            <button className="fechar" onClick={fecharModal}>
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
